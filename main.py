38194f7d03a484b6d9c55e1a5693e5c8b8911286
Gareth Jones <gareth.jones@sainsburys.co.uk>
2019-08-01 12:16:48 +0100

    Announce build failures

--- main.py
+++ main.py
@@ -9,14 +9,24 @@ webhook = Webhook(app) # Defines '/postreceive' endpoint
 def hello_world():
     return "Hello, World!"
 
-@webhook.hook()        # Defines a handler for the 'push' event
-def on_push(data):
-    print("Got push with: {0}".format(data))
-    engine.say("Something happened")
-    engine.runAndWait()
+@webhook.hook(event_type="status")        # Defines a handler for the 'push' event
+def on_status(data):
+    print("Got status with: {0}".format(data))
+    engine = pyttsx3.init()
+
+    if data["state"] == "failure":
+        description = data["description"]
+        author = data["commit"]["commit"]["author"]["name"]
+
+        print(description)
+        print(author)
+
+        engine.say("Oh noes")
+        engine.say(author)
+        engine.say("You broke the build sadface dot jpeg")
+        engine.say(description)
+        engine.runAndWait()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-
-if __name__ == "__main__":
-    engine = pyttsx3.init()
