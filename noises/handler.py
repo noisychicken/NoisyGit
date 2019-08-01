71ec68740221c002570a08efb66115cfe392988a
Gareth Jones <gareth.jones@sainsburys.co.uk>
2019-08-01 15:37:07 +0100

    Handle pending builds

--- noises/handler.py
+++ noises/handler.py
@@ -27,6 +27,11 @@ class Handler:
                 wordsQueue.append(author)
                 wordsQueue.append("Good jorb. You fixed it. You fixed the build - I hope you learnt your lesson")
 
+            if data["state"] == "pending":
+                wordsQueue.append("Oh look some new code by " + author)
+                wordsQueue.append("I hope you know what you're doing dot dot dot")
+                wordsQueue.append("Oh well I better try and build this shit. Hold onto your butts")
+
+
             haha = " ".join(str(x) for x in wordsQueue)
             subprocess.Popen(["python3", "./noises/speak.py", haha])
-
